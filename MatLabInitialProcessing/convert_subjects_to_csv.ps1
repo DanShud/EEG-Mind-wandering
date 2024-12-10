76e37326449378d9108457cd550fdeb11ec5d2e7
KaiBritt <kaibritt27@gmail.com>
2024-12-10 14:30:36 -0500

    commented powershell script

--- MatLabInitialProcessing/convert_subjects_to_csv.ps1
+++ MatLabInitialProcessing/convert_subjects_to_csv.ps1
@@ -1,8 +1,18 @@
-$folder_path = "C:\Users\kaibr\Downloads\MatlabDan"
-$matlabexe = "C:\Users\kaibr\Downloads\MatlabDan\export_cdt_as_csv.exe"
+
+# folder with all mat lab files
+$folder_path = ""
+
+#path to matlab exe
+$matlabexe = ""
 Echo dirname
+
+#getting a list of files
 $files = Get-ChildItem $folder_path
+
+# initialize array
 $subject_file_names = @()
+
+# get the base name for each file store in name e.g "sub_51.mat" - > "sub_51"
 foreach ($f in $files){
 	$len = $f.Name.Length
 	$extension = $f.Name.Substring($len-3)
@@ -10,10 +20,11 @@ foreach ($f in $files){
 		$subject_file_names += $f.Name.Substring(0,$len-4)
 	}
 	
-	Echo $extension 
 }
 Echo $subject_file_names
 $idx = 1
+
+# run matlab exe with the base name as a parameter, spawn child process wait for it to resolve
 foreach ($subject in $subject_file_names){
 	echo "$($idx) of $($subject_file_names.Length)"
 	Start-Process $matlabexe -NoNewWindow -Wait "$($subject)"
